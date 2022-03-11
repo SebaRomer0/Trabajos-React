5d03af014ffd21a668bf35614ddf931c8b2f6e92
Romero Sebastian <sebastian.homero@outlook.com>
2022-03-10 23:24:39 -0300

    Ultimos cambios

--- src/components/itemDetail/cart.jsx
+++ src/components/itemDetail/cart.jsx
@@ -4,16 +4,27 @@ import { useCart } from "../../context/CartContext";
 import { getFirestore } from "../../firebase";
 
 const Cart = () => {
-  const [name,setName] = useState("");
-  const [apell,setApell] = useState("");
-  const [email,SetEmail] = useState("");
-  const [city,setCity] = useState("");
-  const [usuario,setUsuario] = useState("");
-  const [password,setPassword] = useState("");
+  // const [name,setName] = useState("");
+  // const [apell,setApell] = useState("");
+  // const [email,SetEmail] = useState("");
+  // const [city,setCity] = useState("");
+  // const [usuario,setUsuario] = useState("");
+  // const [password,setPassword] = useState("");
+
+  const [formState,setFormState] = useState({
+    nombre:"",
+    apellido:"",
+    email:"",
+    ciudad:"",
+    usuario:"",
+    password:"",
+  })
 
   const { cart, removeItem, clear, newStock, totalPrice } = useCart();
 
   const navegate = useNavigate();
+
+
 
   // const precioTotal =(cart)=>{
   //   let total = 0;
@@ -28,9 +39,9 @@ const Cart = () => {
 
     const newOrder = {
       buyer:{
-        name,
-        apell,
-        city,
+        nombre,
+        apellido,
+        ciudad,
         usuario,
       },
       item:cart,
@@ -88,30 +99,30 @@ const Cart = () => {
                             <h2 id="contacto" className="text-center pt-4 fs-1">Completa Tus Datos</h2>
                             <div className="col-md-6">
                                 <label htmlFor="validationDefault01" className="form-label">Nombre</label>
-                                <input type="text" className="form-control text-center" id="nombre" placeholder="Ingrese su Nombre" value={name} onChange={(e)=> setName(e.target.value)} required />
+                                <input type="text" className="form-control text-center" id="nombre" placeholder="Ingrese su Nombre" value={formState.nombre} onChange={(e)=> setName(e.target.value)} required />
                             </div>
                             <div className="col-md-6">
                                 <label htmlFor="validationDefault02" className="form-label">Apellido</label>
-                                <input type="text" className="form-control text-center" id="apellido" placeholder="Ingrese su Apellido" value={apell} onChange={(e)=> setApell(e.target.value)} required />
+                                <input type="text" className="form-control text-center" id="apellido" placeholder="Ingrese su Apellido" value={formState.apellido} onChange={(e)=> setApell(e.target.value)} required />
                             </div>
                             <div className="col-md-6">
                                 <label htmlFor="validationDefaultUsername" className="form-label">Email</label>
                                 <div className="input-group">
                                     <span className="input-group-text" id="inputGroupPrepend2">@</span>
-                                    <input type="text" className="form-control text-center" id="email" aria-describedby="inputGroupPrepend2" placeholder="Ingrese su Email" value={email} onChange={(e)=> SetEmail(e.target.value)} required />
+                                    <input type="text" className="form-control text-center" id="email" aria-describedby="inputGroupPrepend2" placeholder="Ingrese su Email" value={formState.email} onChange={(e)=> SetEmail(e.target.value)} required />
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <label htmlFor="validationDefault03" className="form-label">Ciudad</label>
-                                <input type="text" className="form-control text-center" id="ciudad" placeholder="Ingrese la Ciudad" value={city} onChange={(e)=> setCity(e.target.value)} required />
+                                <input type="text" className="form-control text-center" id="ciudad" placeholder="Ingrese la Ciudad" value={formState.ciudad} onChange={(e)=> setCity(e.target.value)} required />
                             </div>
                             <div className="col-md-6">
                                 <label htmlFor="validationDefault03" className="form-label ">Usuario</label>
-                                <input type="text" className="form-control text-center" id="usuario" placeholder="Ingrese un Usuario" value={usuario} onChange={(e)=> setUsuario(e.target.value)} required />
+                                <input type="text" className="form-control text-center" id="usuario" placeholder="Ingrese un Usuario" value={formState.usuario} onChange={(e)=> setUsuario(e.target.value)} required />
                             </div>
                             <div className="col-md-6">
                                 <label htmlFor="validationDefault03" className="form-label ">Contraseña</label>
-                                <input type="password" className="form-control text-center" id="contraseña" placeholder="Ingrese una Contraseña" value={password} onChange={(e)=> setPassword(e.target.value)} required />
+                                <input type="password" className="form-control text-center" id="contraseña" placeholder="Ingrese una Contraseña" value={formState.password} onChange={(e)=> setPassword(e.target.value)} required />
                             </div>
                             <div className="col-12">
                                 <button className="btn btn-primary fs-3" type="submit">Finalizar Compra y Registro</button>
